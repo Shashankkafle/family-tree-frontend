0ac5fcc5cdfb1e939fb421c6b0da3262d81d80bd
Shashank Kafle <shashank.kaphle@gmail.com>
2025-06-03 21:49:27 +0700

    add d3-dag for DAG layout and update FamilyTree component to fetch and display data

--- src/components/Dendrogram.js
+++ src/components/Dendrogram.js
@@ -1,166 +1,30 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { Group } from '@visx/group';
 import { LinePath } from '@visx/shape';
-
-import { Cluster, hierarchy } from '@visx/hierarchy';
-import { LinkVertical } from '@visx/shape';
-import { LinearGradient } from '@visx/gradient';
-import PersonCard from './PersonCard';
-import { useParentSize, useScreenSize } from '@visx/responsive';
-
-const citrus = '#ddf163';
-const white = '#ffffff';
-export const green = '#79d259';
-const aqua = '#37ac8c';
-const merlinsbeard = '#f7f7f3';
-export const background = '#306c90';
-const clusterData = 
-	{
-	  "id": 1,
-	  "firstName": "Lokanath",
-	  "parentId": null,
-	  "isRoot": true,
-	  "partnerId": null,
-	  "children": [
-		{
-		  "id": 2,
-		  "firstName": "Mukunda Prashad",
-		  "parentId": 1,
-		  "isRoot": false,
-		  "partnerId": null,
-		  "children": [
-			{
-			  "id": 3,
-			  "firstName": "Sampat Kumar",
-			  "parentId": 2,
-			  "isRoot": false,
-			  "partnerId": 6,
-			  "children": [
-				
-			  ],
-			  "partner": {
-				"id": 6,
-				"firstName": "Subhadra Kumari ",
-				"parentId": null,
-				"isRoot": false,
-				"partnerId": 3,
-				"children": [
-				  {
-					"id": 7,
-					"firstName": "Shashank",
-					"parentId": 3,
-					"isRoot": false,
-					"partnerId": null,
-					"children": [
-					  
-					]
-				  }
-				]
-			  }
-			}
-		  ]
-		}
-	  ]
-	}
-  
-// const clusterData = {
-// 	name: '$',
-// 	children: [
-// 		{
-// 			name: 'A',
-// 			children: [
-// 				{ name: 'A1' },
-// 				{ name: 'A2' },
-// 				{
-// 					name: 'C',
-// 					children: [
-// 						{
-// 							name: 'C1',
-// 						},
-// 					],
-// 				},
-// 			],
-// 		},
-// 		{
-// 			name: 'B',
-// 			children: [{ name: 'B1' }, { name: 'B2' }, { name: 'B3' }],
-// 		},
-// 		{
-// 			name: 'X',
-// 			children: [
-// 				{
-// 					name: 'Z',
-// 				},
-// 			],
-// 		},
-// 	],
-// };
-
-
-
-function RootNode({ node }) {
-	const width = 50;
-	const height = 50;
-	const centerX = -width / 2;
-	const centerY = -height / 2;
-
-	return (
-		<Group top={node.y} left={node.x}>
-			{/* <rect
-				width={width}
-				height={height}
-				y={centerY}
-				x={centerX}
-				fill="url('#top')"
-			/>
-			<text
-				dy=".33em"
-				fontSize={9}
-				fontFamily="Arial"
-				textAnchor="middle"
-				style={{ pointerEvents: 'none' }}
-				fill={background}
-			>
-				{node.data.name}
-			</text> */}
-			asd
-			<rect
-				width={width}
-				height={height}
-				y={centerY}
-				x={centerX}
-				fill="url('#top')"
-			/>
-		</Group>
-	);
-}
+import { graphStratify, sugiyama, layeringSimplex, decrossTwoLayer, coordCenter } from 'd3-dag';
+import axios from 'axios';
 
 function Node({ node }) {
-	const isRoot = node.depth === 0;
-	const isParent = !!node.children;
-	if (isRoot) return <RootNode node={node} />;
-
+	console.log("node.data", node);
 	return (
 		<Group top={node.y} left={node.x}>
-			{node.depth !== 0 && (
+
 				<circle
 					r={12}
-					fill={background}
-					stroke={isParent ? white : citrus}
+					fill='#306c90'
 					onClick={() => {
-						alert(`clicked: ${JSON.stringify(node.data.firstName)}`);
+						alert(`clicked: ${JSON.stringify(node.data.id)}`);
 					}}
 				/>
-			)}
+
 			<text
 				dy=".33em"
 				fontSize={9}
 				fontFamily="Arial"
 				textAnchor="middle"
 				style={{ pointerEvents: 'none' }}
-				fill={isParent ? white : citrus}
 			>
-				{node.data.firstName}
+				{node.data.id}
 			</text>
 		</Group>
 	);
@@ -169,60 +33,76 @@ function Node({ node }) {
 const defaultMargin = { top: 20, left: 20, right: 20, bottom: 20 };
 
 export default function FamilyTree({  margin = defaultMargin }) {
+	const [people, setPeople] = useState([]);
+	async function fetchAllPerson() {
+		const list = await axios.get(process.env.REACT_APP_API_URL + '/person');
+		console.log('list', list.data);
+		setPeople(list.data);
+	}
+
+	useEffect(() => {
+		fetchAllPerson();
+	}, []);
+  const dag = useMemo(() => {
+    if (!people) return null;
+	//Flatten data into a map (id → person)
+	const nodeMap = new Map();
+	for (const person of people) {
+		nodeMap.set(person.id, person);
+	}
+	//Convert to required format: [{ id:int, parentIds:int[] }]
+	const formatted = [];
+	for (const person of nodeMap.values()) {
+		formatted.push({
+			id: String(person.id),
+			parentIds: person.parents?.map(p => String(p.id)) || [],
+			data: person,
+		});
+	}
+	//Build DAG(Directed Acyclic Graph) from formatted structure(this gives us the position of each node)
+	const dag = graphStratify()(formatted);
+
+	//Apply Sugiyama layout
+	const layout = sugiyama()
+	.layering(layeringSimplex())
+	.decross(decrossTwoLayer())
+	.coord(coordCenter())
+	.nodeSize(() => [100, 100]);
+
+	layout(dag)
+	return {
+		nodes: [...dag.nodes()],
+		links: [...dag.links()],
+	  }
+  }, [people]);
+	
 	const chartWidth = 2000;
 	const chartHeight = 1000;
-	// const { parentRef, width, height } = useParentSize({ debounceTime: 150 });	// Inside App component
-	// const [width, height] = useWindowSize();
-	// const [dimensions, setDimensions] = useState({
-    //     width: window.innerWidth,
-    //     height: window.innerHeight,
-    // });
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         setDimensions({
-    //             width: window.innerWidth,
-    //             height: window.innerHeight,
-    //         });
-    //     };
-    //     window.addEventListener('resize', handleResize);
-    //     return () => window.removeEventListener('resize', handleResize);
-    // }, []);
 
-	const data = {"nodes":[{"x":100.5,"y":151,"data":{"id":"child1","parentIds":["p1","p2"]}},{"x":151,"y":50,"data":{"id":"p2"}},{"x":201.5,"y":151,"data":{"id":"child2","parentIds":["p2","p3"]}},{"x":252,"y":50,"data":{"id":"p3"}},{"x":50,"y":50,"data":{"id":"p1"}}],"links":[{"source":1,"target":0,"points":[[151,50],[100.5,151]]},{"source":1,"target":2,"points":[[151,50],[201.5,151]]},{"source":3,"target":2,"points":[[252,50],[201.5,151]]},{"source":4,"target":0,"points":[[50,50],[100.5,151]]}],"v":1}
-	// const data = useMemo(() => hierarchy(clusterData), []);
-	console.log("data",data)
-	const xMax = chartWidth - margin.left - margin.right;
-	const yMax = chartHeight - margin.top - margin.bottom;
-
-	// console.log("1",width,height)
 	return ( 
 			<div style={{
 				width: '100vw',
 				height: '100vh',
 				overflow: 'scroll',
-				background: background,
+				background: '#306c90',
 			}}>
 				<svg width={chartWidth} height={chartHeight}>
-					<LinearGradient id="top" from={green} to={aqua} />
-					<rect width={chartWidth} height={chartHeight} rx={14} fill={background} />
+					<rect width={chartWidth} height={chartHeight} rx={14} fill='#306c90' />
 					<Group top={20} left={20}>
-  {data.links.map((link, i) => (
-    <LinePath
-      key={`link-${i}`}
-      data={link.points}
-      x={d => d[0]}
-      y={d => d[1]}
-      stroke="#ccc"
-      strokeWidth={1.5}
-    />
-  ))}
-  {data.nodes.map((node, i) => (
-    <Group key={`node-${i}`} top={node.y} left={node.x}>
-      <circle r={20} fill="lightblue" />
-      <text dy=".3em" textAnchor="middle">{node.data.id}</text>
-    </Group>
-  ))}
-</Group>
+						{dag.links.map((link, i) => (
+							<LinePath
+							key={`link-${i}`}
+							data={link.points}
+							x={d => d[0]}
+							y={d => d[1]}
+							stroke="#ccc"
+							strokeWidth={1.5}
+							/>
+						))}
+						{dag.nodes.map((node, i) => (
+							<Node node={node} key={`node-${i}`} />
+						))}
+					</Group>
 				</svg>
 			</div>
 		)
