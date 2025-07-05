51fa24807fb4d6e8d58a53b6a33c54ba2dd3ca31
Shashank Kafle <shashank.kaphle@gmail.com>
2025-07-05 16:52:29 +0700

    Refactor denfrogram to use useFetchAllPerson hook for data fetching and add loading state handling

--- src/hooks/apiCalls.js
+++ src/hooks/apiCalls.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
 
 //to fetch all people
 export const useFetchAllPerson = () => {
